98fa14f0d02658f5ac7f895f2d61282118ce46b7
MadhavKhant <khantmadhav33@gmail.com>
2024-12-08 21:12:58 +0530

    t4

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -279,7 +279,9 @@ const Contact = () => {
             <button
               type="submit"
               className="px-2 py-1 bg-gray-500 text-white font-bold text-xl rounded-2xl hover:scale-[90%] 
-                transition-all duration-300 shadow-xl hover:bg-yellow-500 hover:text-blue-600"
+                transition-all duration-300 shadow-xl hover:bg-yellow-500 hover:text-blue-600 w-full h-fit
+                  text-center hover:cursor-pointer
+                "
               onFocus={handleFocus}
               onBlur={handleBlur}
               disabled={loading}
